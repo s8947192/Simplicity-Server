00d9b628082bf64b719869a14526bddf62c091dd
Eugene <frostberryart@gmail.com>
2018-04-20 22:27:00 +0300

    added membership and subscription models

--- common/models/customUser.js
+++ common/models/customUser.js
@@ -1,5 +1,15 @@
-'use strict';
+'use strict'
+
+var app = require('../../server/server')
 
 module.exports = function(CustomUser) {
-
-};
+  CustomUser.afterRemote('create', function(ctx, user, next) {
+    app.models.Subscription.findOrCreate({
+      registrationDate: user.registrationDate,
+      expirationDate: user.registrationDate + 2629743 * 1000,
+      userId: user.id,
+      subscriptionId: "5ad9fc79b3e46f4730c1097b"
+    })
+    next()
+  })
+}
