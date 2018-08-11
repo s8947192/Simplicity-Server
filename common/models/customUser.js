850880e7102905bbc970ac8c47c3deaedece5254
Eugene <frostberryart@gmail.com>
2018-08-11 12:48:12 +0300

    added stripe service, registration flow, nodemon

--- common/models/customUser.js
+++ common/models/customUser.js
@@ -1,15 +1,68 @@
-'use strict'
+const app = require('../../server/server')
 
-// var app = require('../../server/server')
+const stripeClient = require('stripe')('pk_test_U5zfDnUYzX2WoMAMA8BEqNM9')
+const stripe = require('stripe')('sk_test_YVNd7kHmQsZv3jrQYNlQPIQV')
 
 module.exports = function(CustomUser) {
-  // CustomUser.afterRemote('create', function(ctx, user, next) {
-  //   app.models.Subscription.findOrCreate({
-  //     registrationDate: user.registrationDate,
-  //     expirationDate: user.registrationDate + 2629743 * 1000,
-  //     userId: user.id,
-  //     subscriptionId: '5ad9fc79b3e46f4730c1097b'
-  //   })
-  //   next()
-  // })
+
+  CustomUser.observe('before save', async function(ctx) {
+
+    const {
+      StripeService
+    } = app.models
+
+    const {
+      email,
+      username,
+      pricing_plan
+    } = ctx.instance
+
+    const token = await stripeClient.tokens.create({
+      card: {
+        number: '4000000000000077',
+        exp_month: 12,
+        exp_year: 2019,
+        cvc: '123',
+        name: 'USER 1'
+      },
+      email: email
+    })
+
+    let newCustomer = null
+
+    try {
+
+      console.log('checking email...')
+      const takenEmail = await CustomUser.findOne({ where: { email } })
+      if (takenEmail) throw Error(`email ${email} has already been taken`)
+
+      console.log('checking username...')
+      const takenUsername = await CustomUser.findOne({ where: { username } })
+      if (takenUsername) throw Error(`username ${username} has already been taken`)
+
+      console.log('checking customer...')
+      const existedCustomer = await StripeService.getCustomerByEmail(email)
+      if (existedCustomer) throw Error(`customer ${email} has already been registered`)
+
+      console.log('creating new customer...')
+      newCustomer = await StripeService.createCustomer(email)
+
+      console.log('adding payment source...')
+      const paymentSource = await StripeService.createSource(newCustomer.id, token.id)
+
+      console.log('setting default payment source...')
+      await StripeService.setDefaultSource(newCustomer.id, paymentSource.id)
+
+      console.log('subscripbing...')
+      await StripeService.subscribe(newCustomer.id, pricing_plan)
+
+      console.log('starting registration...')
+      await ['token', 'pricing_plan'].forEach(field => ctx.instance.unsetAttribute(field))
+
+    } catch (err) {
+      console.log('cancelling registration...')
+      if (newCustomer) await StripeService.deleteCustomer(newCustomer.id)
+      return Promise.reject(err.message)
+    }
+  })
 }
---
+++ common/models/stripeService.js
@@ -0,0 +1,78 @@
+'use strict'
+
+const stripe = require('stripe')('sk_test_YVNd7kHmQsZv3jrQYNlQPIQV')
+
+module.exports = function(StripeService) {
+
+  StripeService.getSubscriptions = () =>
+    stripe.subscriptions.list()
+
+  StripeService.getPlans = () =>
+    stripe.plans.list()
+
+  StripeService.getCoupons = () =>
+    stripe.coupons.list()
+
+  StripeService.getCustomerByEmail = email =>
+    stripe.customers.list({ limit: 1, email })
+      .then(customers => customers.data[0])
+
+  StripeService.createCustomer = email =>
+    stripe.customers.create({ email })
+
+  StripeService.deleteCustomer = customerId =>
+    stripe.customers.del(customerId)
+
+  StripeService.createSource = (customerId, sourceId) =>
+    stripe.customers.createSource(customerId, { source: sourceId })
+
+  StripeService.setDefaultSource = (customerId, sourceId) =>
+    stripe.customers.update(customerId, { default_source: sourceId })
+
+  StripeService.subscribe = (customer, plan, coupon) =>
+    stripe.subscriptions.create({ customer, coupon, items: [{ plan: plan }] })
+
+}
+
+// const stripeClient = require('stripe')('pk_test_U5zfDnUYzX2WoMAMA8BEqNM9')
+
+// StripeService.charge = async function(msg, cb) {
+//   const token = await stripeClient.tokens.create({
+//     card: {
+//       number: '4242424242424242',
+//       exp_month: 12,
+//       exp_year: 2019,
+//       cvc: '123',
+//       name: 'USER 1'
+//     },
+//     email: 'frostberryart@gmail.com'
+//   })
+//
+//   const plans = await stripe.plans.list()
+//   const coupons = await stripe.coupons.list()
+//   const subscriptions = await stripe.subscriptions.list()
+//
+//   const customer = await stripe.customers.create({
+//     email: token.email
+//   })
+//
+//   const source = await stripe.customers.createSource(customer.id, {
+//     source: token.id
+//   })
+//
+//   const defaultSource = await stripe.customers.update(customer.id, {
+//     default_source: source.id
+//   })
+//
+//   const subscription = await stripe.subscriptions.create({
+//     customer: customer.id,
+//     coupon: coupons.data[0].id,
+//     items: [{ plan: plans.data[0].id }]
+//   })
+//   return 'DONE'
+// }
+
+// StripeService.remoteMethod('createCustomer', {
+//   accepts: { arg: 'email', type: 'string' },
+//   returns: { arg: 'customer', type: 'string' }
+// })
